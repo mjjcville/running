ed88b98ff2b309b21917b05092e1b486fa60714f
Jane Jackson <mjjcville@gmail.com>
2023-11-20 17:03:25 -0500

    Changes to weather module

--- weather.py
+++ weather.py
@@ -3,7 +3,12 @@ from configparser import ConfigParser
 
 
 config = ConfigParser()
-config.read('../config/running_config.cfg')
+config.read('./config/running_config.cfg')
 
 API_KEY = config.get('weatherapi', 'api_key')
+BASE_URL = config.get('weatherapi', 'base_url')
+history_url = BASE_URL + '/history.json?key={}&q={}&dt={}'.format(API_KEY, '22901', '2023-11-19')
 
+response = requests.get(history_url)
+
+print (response)
