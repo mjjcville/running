a2a7eb3f628f3926415394c64dc83d6b2447876e
Jane Jackson <mjjcville@gmail.com>
2023-11-25 14:42:27 -0500

    Adding a drop script for database, fixing a duplicate column name error

---
+++ drop_tables.py
@@ -0,0 +1,18 @@
+from model import database
+import sqlite3
+
+tables:list[str] = [
+    'sleep_history'
+]
+
+database_name:str = "running_journal_app.db"
+
+try:
+    for table in tables:
+        drop_sql_statement: str = "DROP TABLE IF EXISTS " + table
+        runnning_journal_database = database.Database(database_name)
+        runnning_journal_database.execute(drop_sql_statement)
+        runnning_journal_database.close()
+        print (f"Database table {table} dropped successfully.")
+except sqlite3.Error as db_error:
+    print (f"Database Error: {sqlite3.Error.sqlite_errorcode}, {sqlite3.Error.sqlite_errorname}")
